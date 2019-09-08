7afa26d518525048fac36841232e837bf6452e1f
xbary <xbary@wp.pl>
2019-09-08 23:19:52 +0200

    update 19.9.8

--- src/xb_SERIAL.h
+++ src/xb_SERIAL.h
@@ -2,11 +2,20 @@
 #define _XB_SERIAL_h
 
 #include <xb_board.h>
+#include <xb_board_def.h>
 
+#ifndef SERIAL0_SizeRXBuffer
 #define SERIAL0_SizeRXBuffer 128
+#endif
+#ifndef SERIAL1_SizeRXBuffer
 #define SERIAL1_SizeRXBuffer 128
+#endif
+#ifndef SERIAL2_SizeRXBuffer
 #define SERIAL2_SizeRXBuffer 128
+#endif
+#ifndef SERIALBT_SizeRXBuffer
 #define SERIALBT_SizeRXBuffer 128
+#endif
 
 
 extern TTaskDef XB_SERIAL_DefTask;
